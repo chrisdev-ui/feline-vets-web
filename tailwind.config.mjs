65f1120bde212542cf4d84b2dc331a040a6e4e93
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-03-22 22:09:41 -0500

    Update all config files

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    animations,
+    function ({ addVariant }) {
+      addVariant('any-hover', '@media (any-hover: hover) { &:hover }')
+    },
+  ],
+}

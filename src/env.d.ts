1b88732892ac0d34ad2784fdc30a8b7a1081e5e5
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-04-08 17:59:56 -0500

    ✨ New toast component

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,15 @@
 
 interface ImportMetaEnv {
 	readonly INSTAGRAM_ACCESS_TOKEN: string
+	readonly SENDGRID_API_KEY: string
 }
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv
+}
+
+declare global {
+	interface Window {
+		toast: function
+	}
 }

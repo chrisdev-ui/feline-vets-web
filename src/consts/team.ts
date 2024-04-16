e9923e0beb3c8bbf2da66cbc1d5333285f02febb
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-04-16 00:41:49 -0500

    🎨 Fix styles on single member page and add image member for preload it when page is in english

--- src/consts/team.ts
+++ src/consts/team.ts
@@ -8,17 +8,22 @@ export interface Member {
 	years?: number
 }
 
+const calculateYearsOfExperience = (initialYear: number) => {
+	const currentYear = new Date().getFullYear()
+	return currentYear - initialYear
+}
+
 export const allMembers: Member[] = [
 	{
 		id: "paula-morales",
 		image: "https://pub-e0766a16d9da4b039da6c1561ffd52b6.r2.dev/paula-morales.png",
 		customStyle: "shadow-team-1",
-		years: 5,
+		years: calculateYearsOfExperience(2018),
 	},
 	{
 		id: "vanessa-bonilla",
 		image: "https://pub-e0766a16d9da4b039da6c1561ffd52b6.r2.dev/vanessa-bonilla.png",
 		customStyle: "shadow-team-2",
-		years: 5,
+		years: calculateYearsOfExperience(2016),
 	},
 ]
